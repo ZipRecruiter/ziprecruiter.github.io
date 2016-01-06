efd44ff891fd07741a94071232531b2298678480
Gerald burns <gerald@ziprecruiter.com>
2016-01-06 15:22:46 -0800

    Adding some images to display functionality

--- _includes/js/script.js
+++ _includes/js/script.js
@@ -1,5 +1,12 @@
 (function($, undefined){
 
-
+$('body').delegate('.thumb', 'click', function() {
+  var $this = $(this);
+  if ( !$this.is('.collapsed') ) {
+    $this.addClass('collapsed');
+  } else {
+    $this.removeClass('collapsed')
+  }
+});
 
 })(window.jQuery || window.Zepto || window.$);

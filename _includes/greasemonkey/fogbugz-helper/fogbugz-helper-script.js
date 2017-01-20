d924ac4b4e5d778bfeb1957d775567f3a64a1ce1
Gerald burns <gerald@ziprecruiter.com>
2017-01-20 12:45:51 -0800

    Removing console logs

--- _includes/greasemonkey/fogbugz-helper/fogbugz-helper-script.js
+++ _includes/greasemonkey/fogbugz-helper/fogbugz-helper-script.js
@@ -30,11 +30,8 @@ var marked_options = {
 var fb_links_reg = /&lt;a href=&quot;(.+?)&quot; rel=&quot;nofollow noopener noreferrer&quot;(?: (.+?)=&quot;(.+?)&quot;)?&gt;(.+?)&lt;\/a&gt;/g;
 var do_marked = function(text) {
   var ret_text = marked(text, marked_options)
-console.log(ret_text);
 
   ret_text = ret_text.replace(fb_links_reg, '<a href="$1" rel="nofollow noopener noreferrer" $2="$3">$4</a>');
-console.log(ret_text);
-console.log('_____________________________________________________');
 
   return ret_text;
 }

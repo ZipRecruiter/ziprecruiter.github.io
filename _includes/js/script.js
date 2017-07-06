b93c23b2747d786d0859861b4951b87aff2a85c0
Gerald Burns <gerald@ziprecruiter.com>
2017-07-06 14:34:54 -0700

    Emphasizing FogBugz customization

--- _includes/js/script.js
+++ _includes/js/script.js
@@ -1,12 +1,25 @@
 (function($, undefined){
 
-$('body').delegate('.thumb', 'click', function() {
+var $body = $('body');
+
+$body.delegate('.thumb', 'click', function() {
   var $this = $(this);
   if ( !$this.is('.collapsed') ) {
     $this.addClass('collapsed');
   } else {
-    $this.removeClass('collapsed')
+    $this.removeClass('collapsed');
   }
+});
+
+$('[data-type="spoiler"] > :not([data-type="spoiler-info"])').addClass('visually-hidden');
+
+$body.delegate('[data-type="spoiler-show"]', 'click', function() {
+  $(this).closest('[data-type="spoiler"]')
+    .find('> :not([data-type="spoiler-info"])')
+      .removeClass('visually-hidden')
+      .end()
+    .find('> [data-type="spoiler-info"]')
+      .hide();
 });
 
 })(window.jQuery || window.Zepto || window.$);

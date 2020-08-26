8910badbcbe5de4445881aadefd69c3028c3423e
Gerald Burns <gerald@ziprecruiter.com>
2020-08-26 10:10:37 -0700

    Fixing hotfix script command

--- _includes/greasemonkey/jira-helper/jira-helper-script.js
+++ _includes/greasemonkey/jira-helper/jira-helper-script.js
@@ -977,7 +977,7 @@ window.$ = undefined;
 
             if ( text && text.match(/(\b[a-f0-9]{40})/) ) {
               $tooltip.show();
-              $tooltip_text.attr('title', 'Click to copy the hotfix command').html('TSARV_COLORS=1 tsar-v -- bin/zr-req-hotfix -s --qa-facing=yes --release --bugzid=none -m="' + jira_id + '" ' + text + '');
+              $tooltip_text.attr('title', 'Click to copy the hotfix command').html('tsar-v -- tools/starterview_release/bin/sv-hotfix-stg --message="' + jira_id + ' fixes" --qa-facing=yes ' + text + '');
               $tooltip.appendTo(el);
             }
           }, 100);
